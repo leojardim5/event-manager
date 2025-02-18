953b25aebfc9de76c30a86ed477498a44254eec4
leo <leojardim5@gmail.com>
2025-02-18 20:06:49 -0300

    preparando para deixa-lo publico

--- src/main/java/com/leonardo/eventosManager/JavaEstudoApplication.java
+++ src/main/java/com/leonardo/eventosManager/JavaEstudoApplication.java
@@ -3,11 +3,22 @@ package com.leonardo.eventosManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class JavaEstudoApplication {
+
+    static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JavaEstudoApplication.class, args);
     }
- 
 }
