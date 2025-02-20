00ffc881db39d5e08f25374c1fb87bc2fcecb50c
leo <leojardim5@gmail.com>
2025-02-20 18:20:29 -0300

    alterando nomes

--- src/test/java/com/leonardo/javaEstudo/JavaEstudoApplicationTests.java
+++ src/test/java/com/leonardo/javaEstudo/JavaEstudoApplicationTests.java
@@ -3,9 +3,9 @@ package com.leonardo.javaEstudo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.leonardo.eventosManager.JavaEstudoApplication;
+import com.leonardo.eventosManager.EventManagerApplication;
 
-@SpringBootTest(classes = JavaEstudoApplication.class)
+@SpringBootTest(classes = EventManagerApplication.class)
 class JavaEstudoApplicationTests {
 
     @Test
